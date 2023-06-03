60476afa3f4abcf9394a2cf906b9607c4ab0a8ca
engadnanhossain <engr.adnanhossain@gmail.com>
2023-06-03 20:21:02 +0600

    added checkot post

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import { router } from './routes/Routes'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <div className=' w-11/12 mx-auto'>
         <RouterProvider router={router}></RouterProvider>
+        <Toaster/>
     </div>
   )
 }
--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider';
+import { toast } from 'react-hot-toast';
 
 const CheckOut = () => {
-    const {title} = useLoaderData();
+    const { _id, title, price } = useLoaderData();
+    const { user } = useContext(AuthContext);
+
+    const handleCheckOut = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = `${form.fname.value} ${form.lname.value}`;
+        const phone = form.phone.value;
+        const email = user?.email || 'Unregistered';
+        const message = form.message.value;
+
+        const order = {
+            service: _id,
+            serviceName: title,
+            price,
+            customer: name,
+            phone,
+            email,
+            message
+        }
+
+        fetch('http://localhost:5000/orders', {
+            method: 'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.acknowledged){
+                form.reset();
+                toast.success('Your Order Successfull')
+
+            }
+        })
+        .catch(err => console.error(err))
+
+    }
+
     return (
-        <div>
-            <h1>{title}</h1>
+        <div className='m-10'>
+            <h2 className=' text-4xl'>{title}</h2>
+            <form onSubmit={handleCheckOut}>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+                    <input name='fname' type="text" placeholder="First Name" className="input input-bordered w-full" />
+                    <input name='lname' type="text" placeholder="Last Name" className="input input-bordered w-full" />
+                    <input name='phone' type="text" placeholder="Your Phone" className="input input-bordered w-full" />
+                    <input name='email' type="text" placeholder="Your Email" defaultValue={user?.email} readOnly className="input input-bordered w-full" />
+                </div>
+                <textarea name='message' className="textarea textarea-bordered w-full h-24 mt-5" placeholder="Your Message"></textarea>
+                <button className="btn btn-warning mt-5 block mx-auto">Order Confirm</button>
+            </form>
         </div>
     );
 };
